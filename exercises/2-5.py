c04acc5320654edf3deed57e448757cd900175fc
sinclair19 <zsq0219@outlook.com>
2021-08-14 17:54:53 +0800

    section 2

---
+++ exercises/2-5.py
@@ -0,0 +1,10 @@
+first_name = "ada"
+last_name = "lovelace"
+full_name = f"{first_name} {last_name}"
+message = f"Hello, {full_name.title()}!"
+print(message)
+
+famous_person = "Albert Einstein"
+text = '"A person who never made a mistake never tried anything new."'
+message = f"{famous_person} once said, {text}"
+print(message)
